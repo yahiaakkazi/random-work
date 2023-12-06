2c605467014c98ef77b63ff0caa9e1af022d79e0
Yahia AKKAZI <yahyaakkazi@gmail.com>
2023-12-06 15:18:51 +0100

    Adding the producer app repo into the same repo, adding the docker-compose file for Kafka, and editing the README.md for further information

---
+++ kafka-producer/src/main/java/com/example/PushToTopic.java
@@ -0,0 +1,30 @@
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class PushToTopic {
+    public static void main(String[] args) {
+        String bootstrapServers = "localhost:29092";
+        String topic = "test-topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        try {
+            String message = "{\"content\": \"test\"}";
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+            producer.send(record);
+            System.out.println("Message sent successfully: " + message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+    }
+}
